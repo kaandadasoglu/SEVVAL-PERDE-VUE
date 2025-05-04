682a53d22f19ef600994674160cc3e4d5242b48b
Kaan Dadaşoğlu <kaandadasoglu@gmail.com>
2025-05-04 13:05:01 +0300

    Update InfoKatlamaliPerde.vue

--- src/views/info/InfoKatlamaliPerde.vue
+++ src/views/info/InfoKatlamaliPerde.vue
@@ -17,7 +17,9 @@ useHead({
 
 <template>
   <div class="container page-container info-page">
-    <h1>Katlamalı Perdeler (Roman Shade): Şıklık ve Zarafet</h1>
+    <h1>
+      Katlamalı Perdeler (Roman Shade): Mekanlarınıza Şıklık ve Zarafeti Taşıyın
+    </h1>
 
     <div class="info-content">
       <div class="info-image">
@@ -25,46 +27,278 @@ useHead({
       </div>
       <div class="info-text">
         <p>
-          Katlamalı perdeler, kumaşın yukarı doğru toplandığında zarif katmanlar
-          oluşturduğu, estetik ve dekoratif bir perde türüdür. Mekanizması
-          sayesinde stor perdeler gibi pratik bir kullanım sunarken, kumaşın
-          sıcaklığı ve şıklığıyla da öne çıkarlar.
+          Pencere dekorasyonunda estetik ve zarafeti bir arada arayanlar için
+          katlamalı perde, diğer adıyla Roman Shade, kumaşın yukarı doğru
+          toplandığında oluşturduğu zarif katmanlarla öne çıkan özel bir perde
+          türüdür. Stor perde gibi pratik bir kullanım sunarken, seçilen kumaşın
+          sıcaklığı ve şıklığıyla mekanlara bambaşka bir hava katar. İster
+          klasik, ister modern bir tarz benimsemiş olun, katlamalı perde
+          modelleri evinize veya ofisinize sofistike bir dokunuş katmanın ideal
+          yoludur. İstanbul ve özellikle Kadıköy bölgesinde evinize zarafet
+          katacak birbirinden şık katlamalı perde seçenekleri için Şevval Perde
+          mağazamızı ziyaret edebilir veya bizimle iletişime geçebilirsiniz.
         </p>
-        <h2>Katlamalı Perdelerin Özellikleri</h2>
+        <h2>
+          Katlamalı Perde (Roman Shade) Nedir? Kumaşın Zarif Katmanları ile
+          Estetik Bir Dokunuş
+        </h2>
+        <p>
+          Katlamalı perde, düz bir kumaş panelinin, mekanizması sayesinde yukarı
+          doğru düzenli ve zarif katmanlar oluşturarak toplanması prensibiyle
+          çalışan bir perde sistemidir. Bu katmanlı yapı, pencereye sadece
+          işlevsel bir örtü olmanın ötesinde, dekoratif ve estetik bir boyut
+          kazandırır. Kumaş perde kategorisinde öne çıkan katlamalı perdeler,
+          seçilen kumaşın dokusu, rengi ve deseniyle mekânın atmosferini önemli
+          ölçüde etkileyebilir. Roman Shade olarak da bilinen bu perdeler hem
+          modern hem de klasik dekorasyon tarzlarına kolayca uyum sağlayabilen
+          zamansız bir şıklık sunar.
+        </p>
+        <h2>
+          Katlamalı Perdelerin Benzersiz Özellikleri: Neden Katlamalı Perde
+          Seçmelisiniz?
+        </h2>
         <ul>
           <li>
-            <strong>Dekoratif Görünüm:</strong> Toplandığında oluşan katmanlı
-            yapı, pencereye estetik bir boyut katar.
+            <h3>
+              Dekoratif ve Zarif Görünüm: Toplandığında Oluşan Estetik Katmanlar
+            </h3>
+            <p>
+              Katlamalı perdelerin en dikkat çekici özelliklerinden biri, yukarı
+              doğru toplandıklarında oluşturdukları düzenli ve zarif
+              katmanlardır. Bu katmanlı yapı, pencereye estetik bir derinlik ve
+              görsel bir zenginlik katarak mekanın genel atmosferini
+              güzelleştirir.
+            </p>
           </li>
           <li>
-            <strong>Kumaş Çeşitliliği:</strong> Tül, pamuklu, keten, sentetik
-            gibi birçok farklı kumaş türüyle üretilebilirler. Bu sayede
-            istenilen ışık geçirgenliği ve görünüm elde edilebilir.
+            <h3>
+              Zengin Kumaş Çeşitliliği: Her Tarza ve İhtiyaca Uygun Seçenekler
+            </h3>
+            <p>
+              Katlamalı perdeler, tül, pamuklu, keten, sentetik gibi birçok
+              farklı kumaş türüyle üretilebilir. Bu geniş kumaş perde yelpazesi,
+              istenilen ışık geçirgenliğini (ışık geçiren, yarı karartma, tam
+              karartma) ve mekanın dekorasyon tarzına uygun görünümü elde etme
+              imkanı sunar.
+            </p>
           </li>
           <li>
-            <strong>Yer Tasarrufu:</strong> Pencere boşluğuna veya dışına monte
-            edilebilirler ve toplandıklarında az yer kaplarlar.
+            <h3>Yer Tasarrufu Sağlayan Tasarım: Pratik ve Şık Çözümler</h3>
+            <p>
+              Katlamalı perdeler, pencere boşluğuna veya dışına kolayca monte
+              edilebilir ve toplandıklarında diğer perde türlerine göre daha az
+              yer kaplar. Bu yer tasarrufu sağlayan tasarım, özellikle sınırlı
+              alana sahip mekanlar için pratik ve şık bir çözüm sunar.
+            </p>
           </li>
           <li>
-            <strong>Kolay Kullanım:</strong> Zincirli veya ipli mekanizmaları
-            sayesinde kolayca açılıp kapanabilirler. Motorlu seçenekleri de
-            mevcuttur.
+            <h3>Kolay ve Konforlu Kullanım: Zahmetsiz Kontrol İmkanı</h3>
+            <p>
+              Katlamalı perdeler, genellikle zincirli veya ipli mekanizmalar
+              sayesinde kolayca açılıp kapanabilir. Bu kullanıcı dostu
+              mekanizmalar, perdenin istenilen seviyede sabitlenmesine olanak
+              tanır.
+            </p>
+          </li>
+          <li>
+            <h3>Motorlu Seçenekler: Modern Yaşamın Lüksü ve Rahatlığı</h3>
+            <p>
+              Modern yaşamın konforunu sunan motorlu katlamalı perde seçenekleri
+              de mevcuttur. Bu sayede, tek bir tuşla veya uzaktan kumanda
+              aracılığıyla perdelerinizi zahmetsizce kontrol edebilirsiniz.
+            </p>
           </li>
         </ul>
-        <h2>Kullanım Alanları ve Dekorasyon İpuçları</h2>
+        <h2>
+          Katlamalı Perdelerin Geniş Kullanım Alanları ve Dekorasyon İpuçları:
+          Evin Her Köşesinde Zarafet
+        </h2>
         <p>
-          Katlamalı perdeler, klasik, modern veya rustik fark etmeksizin birçok
-          dekorasyon tarzına uyum sağlayabilir. Özellikle salon, oturma odası ve
-          yatak odalarında şık bir tercih olabilirler. Mutfaklarda da
-          kullanılabilirler ancak kumaş seçimine dikkat edilmelidir. Kumaş ve
-          renk seçimi, odanın genel atmosferini belirlemede önemli rol oynar.
+          Katlamalı perdeler, şık ve zarif görünümleri sayesinde evin birçok
+          farklı alanında tercih edilebilir:
         </p>
-        <h2>Şevval Perde'de Katlamalı Perde Seçenekleri</h2>
+        <ul>
+          <li>
+            <h3>Salon ve Oturma Odalarında Klasik ve Modern Şıklık</h3>
+            <p>
+              Salon perdesi ve oturma odası perdesi olarak katlamalı perdeler,
+              hem klasik hem de modern dekorasyon tarzlarına kolayca uyum
+              sağlar. Seçilen kumaş ve desenle mekanın atmosferini
+              zenginleştirebilirsiniz.
+            </p>
+          </li>
+          <li>
+            <h3>Yatak Odalarında Sıcak, Davetkar ve Romantik Bir Atmosfer</h3>
+            <p>
+              Yatak odası perdesi olarak katlamalı perdeler sadece pratiklik
+              değil aynı zamanda şık bir görüntü de sağlar. Üstelik karartma
+              özellikli kumaşlarla uyku kalitenizi artırabilirsiniz.
+            </p>
+          </li>
+          <li>
+            <h3>Yer Tasarrufu Sağlayan Tasarım: Pratik ve Şık Çözümler</h3>
+            <p>
+              Katlamalı perdeler, pencere boşluğuna veya dışına kolayca monte
+              edilebilir ve toplandıklarında diğer perde türlerine göre daha az
+              yer kaplar. Bu yer tasarrufu sağlayan tasarım, özellikle sınırlı
+              alana sahip mekanlar için pratik ve şık bir çözüm sunar.
+            </p>
+          </li>
+          <li>
+            <h3>Kolay ve Konforlu Kullanım: Zahmetsiz Kontrol İmkanı</h3>
+            <p>
+              Katlamalı perdeler, genellikle zincirli veya ipli mekanizmalar
+              sayesinde kolayca açılıp kapanabilir. Bu kullanıcı dostu
+              mekanizmalar, perdenin istenilen seviyede sabitlenmesine olanak
+              tanır.
+            </p>
+          </li>
+          <li>
+            <h3>Mutfaklarda Pratik ve Şık Çözümler (Kumaş Seçimine Dikkat)</h3>
+            <p>
+              Mutfaklarda da katlamalı perdeler kullanılabilir ancak nem ve
+              lekelenmeye dayanıklı kumaşlar tercih edilmelidir.
+            </p>
+          </li>
+          <li>
+            <h3>Çalışma Odalarında Zarif ve Dikkat Dağıtmayan Bir Ortam</h3>
+            <p>
+              Çalışma odalarında kullanılan katlamalı perdeler, zarif
+              görünümleriyle dikkat dağıtmayan, sade ve şık bir ortam yaratır.
+            </p>
+          </li>
+          <li>
+            <h3>Diğer Yaşam Alanlarında Estetik ve Tamamlayıcı Bir Unsur</h3>
+            <p>
+              Koridorlarda, misafir odalarında ve diğer yaşam alanlarında da
+              katlamalı perdeler, estetik ve tamamlayıcı bir unsur olarak
+              kullanılabilir.
+            </p>
+          </li>
+        </ul>
+        <h2>
+          Katlamalı Perde Bakımı Nasıl Yapılır? Kumaş Türüne Göre Pratik
+          Temizlik Yöntemleri
+        </h2>
+        <p>
+          Katlamalı perde bakımı, seçilen kumaşın türüne göre değişiklik
+          gösterebilir. Ancak genel olarak dikkat etmeniz gereken bazı önemli
+          noktalar şunlardır:
+        </p>
+        <ul>
+          <li>
+            <p>
+              <strong>Kumaşa Uygun Yıkama veya Kuru Temizleme:</strong>
+              Perdelerinizi yıkamanız gerektiğinde, kumaşın türüne uygun yıkama
+              talimatlarına dikkat edin. Hassas kumaşlar için kuru temizleme
+              önerilebilir. Mekanizmadan kumaşları ayırmak ve seçtiğiniz tüle ya
+              da kumaşa göre yıkama talimatlarını Şevval Perde ekibimizden
+              detaylarıyla öğrenebilirsiniz.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Ütüleme:</strong> Yıkama sonrası perdelerinizi ütülerken,
+              kumaşın cinsine uygun ısı ayarını kullanın ve gerekirse üzerine
+              nemli bir bez sererek ütüleyin.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Leke Çıkarma:</strong> Leke oluştuğunda, hızlı bir şekilde
+              ve kumaşa uygun bir leke çıkarıcı ile müdahale edin.
+            </p>
+          </li>
+        </ul>
+        <h2>
+          Katlamalı Perde Seçerken Nelere Dikkat Etmelisiniz? Doğru Karar İçin
+          Önemli Kriterler
+        </h2>
+        <p>
+          Katlamalı perde seçimi yaparken mekanınızın ihtiyaçlarını ve tarzını
+          göz önünde bulundurmanız önemlidir:
+        </p>
+        <ul>
+          <li>
+            <h3>Mekanın Dekorasyon Tarzı ve Atmosferi</h3>
+            <p>
+              Seçtiğiniz katlamalı perde modeli, mekanınızın genel dekorasyon
+              tarzıyla uyumlu olmalıdır. Neyseki her türlü kumaş ile
+              üretilebilen katlamalı perdelerde kumaş seçimi ve ev dekorasyon
+              tarzınızı kombinlemek gayet rahattır.
+            </p>
+          </li>
+          <li>
+            <h3>İstenen Işık Geçirgenliği ve Mahremiyet Düzeyi</h3>
+            <p>
+              Mekanın ihtiyacına göre ışık geçiren, yarı karartma veya tam
+              karartma özellikli kumaşları tercih edebilirsiniz.
+            </p>
+          </li>
+          <li>
+            <h3>Yer Tasarrufu Sağlayan Tasarım: Pratik ve Şık Çözümler</h3>
+            <p>
+              Katlamalı perdeler, pencere boşluğuna veya dışına kolayca monte
+              edilebilir ve toplandıklarında diğer perde türlerine göre daha az
+              yer kaplar. Bu yer tasarrufu sağlayan tasarım, özellikle sınırlı
+              alana sahip mekanlar için pratik ve şık bir çözüm sunar.
+            </p>
+          </li>
+          <li>
+            <h3>Kumaşın Türü ve Özellikleri</h3>
+            <p>
+              Kumaşın dokusu, deseni ve özellikleri perdenin görünümünü ve
+              kullanımını etkiler.
+            </p>
+          </li>
+          <li>
+            <h3>Mekanizma Seçimi ve Kullanım Kolaylığı</h3>
+            <p>
+              Kullanım alışkanlıklarınıza uygun bir mekanizma türü seçmek
+              önemlidir. Motorlu seçenekler konforu artırır.
+            </p>
+          </li>
+          <li>
+            <h3>Pencerenin Boyutları ve Montaj Şekli</h3>
+            <p>
+              Pencerenizin boyutları ve montaj şekli, doğru perde ölçülerini
+              belirlemede önemlidir.
+            </p>
+          </li>
+          <li>
+            <h3>Bütçe ve Uzun Vadeli Estetik</h3>
+            <p>
+              Bütçenizi belirleyin ve uzun vadede şık ve dayanıklı bir ürün
+              seçmeye özen gösterin.
+            </p>
+          </li>
+        </ul>
+        <h2>
+          Sonuç: Katlamalı Perdeler ile Mekanlarınıza Zamansız Bir Zarafet Katın
+        </h2>
+        <p>
+          Katlamalı perdeler (Roman Shade), mekanlarınıza zamansız bir zarafet
+          ve şıklık katmanın eşsiz bir yoludur. Kumaşın zarif katmanları, her
+          türlü dekorasyon tarzına uyum sağlayabilen estetik bir görünüm
+          sunarken, pratik kullanımı ve zengin kumaş seçenekleriyle de
+          fonksiyonel bir çözüm sunar. İstanbul Kadıköy'deki Şevval Perde
+          mağazamızda, evinize değer katacak birbirinden özel katlamalı perde
+          modellerini keşfedin ve yaşam alanlarınıza sofistike bir dokunuş
+          katın!
+        </p>
+        <h2>
+          Şevval Perde'de Özel Katlamalı Perde Seçenekleri (İstanbul Kadıköy):
+          Zarafetin Adresi
+        </h2>
         <p>
           Kadıköy Şevval Perde olarak, evinize zarafet katacak birbirinden şık
-          katlamalı perde modellerini sunuyoruz. Kaliteli kumaşlar, dayanıklı
-          mekanizmalar ve isteğinize özel üretim imkanları için mağazamıza
-          uğrayabilir veya bizimle iletişime geçebilirsiniz.
+          katlamalı perde modellerini sunmaktan gurur duyuyoruz. Kaliteli
+          kumaşlardan üretilmiş, dayanıklı mekanizmalara sahip ve isteğinize
+          özel ölçülerde üretilebilen katlamalı perde seçeneklerimizle
+          hayallerinizdeki perdeye kavuşmanızı sağlıyoruz. Uzman ekibimiz, kumaş
+          seçimi, ölçü alma ve montaj konularında size profesyonel destek sunmak
+          için hazır. Mağazamıza uğrayarak veya bizimle iletişime geçerek özel
+          katlamalı perde koleksiyonumuzu keşfedebilirsiniz.
         </p>
         <div class="actions">
           <RouterLink
